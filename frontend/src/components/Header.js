2bf7c905615cfb59d4afab1d6f4fbda81bcaeb2c
ra9g16 <akyu00627@gmail.com>
2021-04-20 14:32:34 +0900

    Frontend User Authentication

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { useDispatch, useSelector} from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    const logoutHandler = () => {
+      dispatch(logout())
+    }
+
     return (
         <header>
           <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
             <Container>
-              <Navbar.Brand as={Link} to="/">ユアカレー</Navbar.Brand>
+              <LinkContainer to = '/'><Navbar.Brand>ユアカレー</Navbar.Brand></LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ml-auto">
-                <Nav.Link as={Link} to="/cart"><i className='fas fa-shopping-cart'></i>カート</Nav.Link>
-                <Nav.Link as={Link} to="/login"><i className='fas fa-user'></i>サインイン</Nav.Link>
+                <LinkContainer to='/cart'>
+                  <Nav.Link><i className='fas fa-shopping-cart'></i>カート</Nav.Link>
+                </LinkContainer>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>登録情報＆注文</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>ログアウト</NavDropdown.Item>
+                  </NavDropdown>
+                ) : <LinkContainer to='/login'>
+                <Nav.Link><i className='fas fa-user'></i>ログイン</Nav.Link>
+              </LinkContainer>}
               </Nav>
               </Navbar.Collapse>
             </Container>
