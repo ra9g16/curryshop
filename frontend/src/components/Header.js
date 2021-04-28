319cc2d9cd6db6b020ed64bbc3d13eac41b51111
ra9g16 <akyu00627@gmail.com>
2021-04-28 20:59:04 +0900

    Nav bar and Images

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { logout } from '../actions/userActions'
-import logo from './logo.png'
+import logo from '../images/logo.png'
 
 const Header = () => {
   const dispatch = useDispatch()
@@ -20,7 +20,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <a href='/' target='blank'>
+            <a className='logoContainer' href='/' target='blank'>
               <img src={logo} width='50' height='50' alt='Logo' />
             </a>
           </LinkContainer>
@@ -33,33 +33,40 @@ const Header = () => {
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
-                <NavDropdown title={userInfo.name} id='username'>
-                  <LinkContainer to='/profile'>
-                    <NavDropdown.Item>登録情報＆注文</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    ログアウト
-                  </NavDropdown.Item>
-                </NavDropdown>
+                !userInfo.isAdmin ? (
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>登録情報＆注文</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      ログアウト
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                ) : (
+                  <NavDropdown title={userInfo.name} id='adminmenu'>
+                    <LinkContainer to='/admin/userlist'>
+                      <NavDropdown.Item>ユーザーリスト</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to='/admin/productlist'>
+                      <NavDropdown.Item>製品リスト</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to='/admin/orderlist'>
+                      <NavDropdown.Item>注文リスト</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>登録情報</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      ログアウト
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                )
               ) : (
                 <LinkContainer to='/login'>
                   <Nav.Link>
                     <i className='fas fa-user'></i>ログイン
                   </Nav.Link>
                 </LinkContainer>
-              )}
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminmenu'>
-                  <LinkContainer to='/admin/userlist'>
-                    <NavDropdown.Item>ユーザーリスト</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/admin/productlist'>
-                    <NavDropdown.Item>製品リスト</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/admin/orderlist'>
-                    <NavDropdown.Item>注文リスト</NavDropdown.Item>
-                  </LinkContainer>
-                </NavDropdown>
               )}
             </Nav>
           </Navbar.Collapse>
